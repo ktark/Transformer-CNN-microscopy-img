f4afc045c99e6855ad40dbb25c3f72edb54b3031
kaarel.tark <kaareltark@gmail.com>
2021-05-01 18:44:58 +0300

    removed un-needed files

--- hello_world.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -J hello_world
-#SBATCH --partition=testing
-#SBATCH -t 1:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=500
-# your code goes below
-module load python/3.8.6
-python -c 'print ("Hello world! Kaarel")'
