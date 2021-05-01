7ab367342b9f6f5b22c2ad8cf5fd01a34caca634
kaarel.tark <kaareltark@gmail.com>
2021-05-01 18:52:31 +0300

    university dataset training setup

--- TransUNet/start_training_uni.sh
+++ TransUNet/start_training_uni.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J NN_TransUnet2
 #SBATCH --partition=gpu
-#SBATCH -t 00:10:00
+#SBATCH -t 12:00:00
 # your code goes below
 module load python-3.7.1
 source NNproject/bin/activate
